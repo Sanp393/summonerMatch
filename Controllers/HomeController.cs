3cea2af3d6013f1105165b6bddaafb98320842fa
IliasVilux <iliasotsman@gmail.com>
2023-12-12 16:29:28 +0100

    register view form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,25 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SummonerMatch.Models;
+using System.Diagnostics;
 
-namespace SummonerMatch._Controllers
+namespace SummonerMatch.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger)
         {
-            _context = context;
+            _logger = logger;
         }
 
         public IActionResult Index()
         {
-            List<Partida> Partidas = _context.Partidas.ToList();
-            return View(Partidas);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
