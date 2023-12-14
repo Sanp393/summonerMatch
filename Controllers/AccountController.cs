ef99aec0a685e53abb1535b8a2d3f1a9ecb77ca6
IliasVilux <iliasotsman@gmail.com>
2023-12-14 16:20:06 +0100

    user saved in session for 24h when loggin

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json;
 
 namespace SummonerMatch
 {
@@ -26,9 +27,7 @@ namespace SummonerMatch
 
             if (usuario != null)
             {
-                bool usuarioAutenticado = true;
-                ViewBag.UsuarioAutenticado = usuarioAutenticado;
-
+                HttpContext.Session.SetObject("Usuario", usuario);
                 return RedirectToAction("Index", "Home");
             }
             else
@@ -78,6 +77,21 @@ namespace SummonerMatch
             _context.SaveChanges();
 
             return RedirectToAction("Login");
+        }
+    }
+
+    public static class SessionExtensions
+    {
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            var json = JsonSerializer.Serialize(value);
+            session.SetString(key, json);
+        }
+
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var json = session.GetString(key);
+            return json == null ? default : JsonSerializer.Deserialize<T>(json);
         }
     }
 }
