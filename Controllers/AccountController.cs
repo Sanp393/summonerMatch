3748307ac4e6da2045e694ff7c42be21e1c2b777
Eric Naranjo Olmo <eric.personal.150@gmail.com>
2023-12-15 15:16:11 +0100

    Algunas correcciones

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -67,7 +67,7 @@ namespace SummonerMatch
                 Contrasena = contrasena,
                 FkRegion = region,
                 FkRango = rango,
-                FkPosicion = posicion
+                FkPosicion = posicion,
                 ImagenPerfil = "LogoLoL",
             };
 
@@ -81,7 +81,7 @@ namespace SummonerMatch
         public IActionResult Profile()
         {
             ViewData["Rangos"] = _context.Rango.ToList();
-            ViewData["Regiones"] = _context.RegionServidor.ToList();
+            ViewData["Regiones"] = _context.Region.ToList();
             ViewData["Posiciones"] = _context.Posicion.ToList();
             return View();
         }
@@ -91,11 +91,11 @@ namespace SummonerMatch
         {        
             var usuario = HttpContext.Session.GetObject<Usuario>("Usuario");
 
-            usuario.userNickname = nickname;
-            usuario.fkRango = rango;
-            usuario.fkRegionServidor = region;
-            usuario.fkPosicion = posicion;
-            usuario.imagenPerfil = imgPerfil;
+            usuario.NombreUsuario = nickname;
+            usuario.FkRango = rango;
+            usuario.FkRegion = region;
+            usuario.FkPosicion = posicion;
+            usuario.ImagenPerfil = imgPerfil;
 
             HttpContext.Session.SetObject("Usuario", usuario);
             _context.SaveChanges();
