8883f5b56030d0d6d44cfeaf8fb9d5f763dc6708
DavidMarchanDelPino <david.marchan.devops@gmail.com>
2023-12-19 18:29:09 +0100

    # eliminar unirse i abandonar

--- Controllers/PartidaController.cs
+++ Controllers/PartidaController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SummonerMatch
 {
@@ -16,6 +17,39 @@ namespace SummonerMatch
         {
             var partidaBuscada = _context.Partida.FirstOrDefault(partida => partida.IdPartida == Id);
             return View(partidaBuscada);
+        }
+
+        public IActionResult UnirsePosicion(int idPartida, string posicion, string usuarioLoL)
+        {
+            var partidaBuscada = _context.Partida.FirstOrDefault(partida => partida.IdPartida == idPartida);
+
+            switch (posicion)
+            {
+                case "top":
+                    partidaBuscada.JugadorTop = usuarioLoL;
+                    partidaBuscada.NumJugadores++;
+                    break;
+                case "jungle":
+                    partidaBuscada.JugadorJungle = usuarioLoL;
+                    partidaBuscada.NumJugadores++;
+                    break;
+                case "mid":
+                    partidaBuscada.JugadorMid = usuarioLoL;
+                    partidaBuscada.NumJugadores++;
+                    break;
+                case "support":
+                    partidaBuscada.JugadorSupport = usuarioLoL;
+                    partidaBuscada.NumJugadores++;
+                    break;
+                case "adc":
+                    partidaBuscada.JugadorAdc = usuarioLoL;
+                    partidaBuscada.NumJugadores++;
+                    break;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("DetallesPartida", "Partida", new { Id = idPartida });
         }
 
         [HttpPost]
@@ -45,5 +79,33 @@ namespace SummonerMatch
             return RedirectToAction("DetallesPartida", "Partida", new { id = nuevaPartida.IdPartida });
         }
 
+        [HttpPost]
+        public IActionResult AbandonarPartida(int idPartida)
+        {
+            var partidaAbandonada = _context.Partida.FirstOrDefault(partida => partida.IdPartida == idPartida);
+            var UsuarioLol = HttpContext.Session.GetObject<Usuario>("Usuario");
+
+            partidaAbandonada.JugadorTop = (partidaAbandonada.JugadorTop == UsuarioLol.UsuarioLoL) ? null : partidaAbandonada.JugadorTop;
+            partidaAbandonada.JugadorMid = (partidaAbandonada.JugadorMid == UsuarioLol.UsuarioLoL) ? null : partidaAbandonada.JugadorMid;
+            partidaAbandonada.JugadorSupport = (partidaAbandonada.JugadorSupport == UsuarioLol.UsuarioLoL) ? null : partidaAbandonada.JugadorSupport;
+            partidaAbandonada.JugadorJungle = (partidaAbandonada.JugadorJungle == UsuarioLol.UsuarioLoL) ? null : partidaAbandonada.JugadorJungle;
+            partidaAbandonada.JugadorAdc = (partidaAbandonada.JugadorAdc == UsuarioLol.UsuarioLoL) ? null : partidaAbandonada.JugadorAdc;
+
+            partidaAbandonada.NumJugadores--;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        public IActionResult EliminarPartida(int idPartida)
+        {
+            var partidaEliminada = _context.Partida.FirstOrDefault(partida => partida.IdPartida == idPartida);
+            _context.Partida.Remove(partidaEliminada);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
